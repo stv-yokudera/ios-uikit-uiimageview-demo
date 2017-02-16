969253ad0abb254c6005f52bfb6aa04c2a9ed7a4
taguchi-k <k.taguchi@st-ventures.jp>
2017-02-17 01:01:50 +0900

    Add SampleCode

--- ios-uikit-uiimageview-demo/ViewController.swift
+++ ios-uikit-uiimageview-demo/ViewController.swift
@@ -10,16 +10,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var subView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupSubView()
+        
+        /*
+         UIViewContentModeのデフォルト値
+         　↓
+         scaleToFill：アスペクト比を変更してviewのサイズにフィットさせる
+         */
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func setupSubView() {
+        subView.layer.borderWidth = 1.0
+        subView.layer.borderColor = UIColor.blue.cgColor
+        subView.layer.cornerRadius = 10.0
     }
-
-
+    
+    // MARK: - option
+    
+    // 画像を設定する
+    private func configureImage() {
+        imageView.image = #imageLiteral(resourceName: "kobaton")
+    }
+    
+    /*
+     アスペクト比を維持してviewのサイズにフィットさせる（Storyboardで設定可能）
+     画像がviewのサイズより小さくなることもある
+     */
+    private func configureScaleAspectFit() {
+        imageView.contentMode = .scaleAspectFit
+    }
+    
+    /*
+     アスペクト比を維持してviewのサイズいっぱいに表示する（Storyboardで設定可能）
+     viewから画像がはみ出ることもある
+     */
+    private func configureScaleAspectFill() {
+        imageView.contentMode = .scaleAspectFill
+    }
+    
+    /*
+     configureScaleAspectFill()を実行 または 
+     StoryboardでimageViewのContentModeをAspectFill にしてから
+     下記メソッドを実行すること
+     */
+    
+    /*
+     画像がviewからはみ出さないようにするには、土台のview側を設定する
+     */
+    // 画像がviewからはみ出た部分を表示しないようにする①（Storyboardで設定可能）
+    func configureClipsToBounds() {
+        subView.clipsToBounds = true
+    }
+    
+    // 画像がviewからはみ出た部分を表示しないようにする②
+    private func configureMasksToBounds() {
+        subView.layer.masksToBounds = true
+    }
 }
 
